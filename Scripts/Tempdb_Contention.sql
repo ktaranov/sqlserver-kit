98df5a642b34b280bd34e0205862f3795079217c
ktaranov <kast218@gmail.com>
2016-11-04 09:17:18 +0300

    [FEATURE] Add Temdb Contention Script

---
+++ Scripts/Tempdb_Contention.sql
@@ -0,0 +1,36 @@
+/*
+Author: Paul Randal
+Original link: http://www.sqlskills.com/blogs/paul/the-accidental-dba-day-27-of-30-troubleshooting-tempdb-contention/
+*/
+
+SELECT
+    [owt].[session_id],
+    [owt].[exec_context_id],
+    [owt].[wait_duration_ms],
+    [owt].[wait_type],
+    [owt].[blocking_session_id],
+    [owt].[resource_description],
+    CASE [owt].[wait_type]
+        WHEN N'CXPACKET' THEN
+            RIGHT ([owt].[resource_description],
+            CHARINDEX (N'=', REVERSE ([owt].[resource_description])) - 1)
+        ELSE NULL
+    END AS [Node ID],
+    [es].[program_name],
+    [est].text,
+    [er].[database_id],
+    [eqp].[query_plan],
+    [er].[cpu_time]
+FROM sys.dm_os_waiting_tasks [owt]
+INNER JOIN sys.dm_exec_sessions [es] ON
+    [owt].[session_id] = [es].[session_id]
+INNER JOIN sys.dm_exec_requests [er] ON
+    [es].[session_id] = [er].[session_id]
+OUTER APPLY sys.dm_exec_sql_text ([er].[sql_handle]) [est]
+OUTER APPLY sys.dm_exec_query_plan ([er].[plan_handle]) [eqp]
+WHERE
+    [es].[is_user_process] = 1
+ORDER BY
+    [owt].[session_id],
+    [owt].[exec_context_id];
+GO
