b79c98a52388f9d5a9936dbaabd94adbc5f4caf3
ktaranov <kast218@gmail.com>
2017-01-12 00:13:08 +0300

    Update tempdb information script

--- Scripts/tempdb_Information.sql
+++ Scripts/tempdb_Information.sql
@@ -3,34 +3,61 @@ Author: Eric Russell
 Original link: http://www.sqlservercentral.com/scripts/tempdb/151252/
 */
 -- Query details about objects allocated in TEMPDB. This must be run in context of TEMPDB database.
+SET LOCK_TIMEOUT 10000;
+SET DEADLOCK_PRIORITY LOW;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
 USE tempdb;
 
-SELECT DISTINCT DB_NAME() AS database_name
-     , o.type_desc object_type
-     , o.name AS object_name
-     , o.create_date AS object_created
-     , si.name as index_name
-     , case si.index_id
-        when 0 then 'HEAP'
-        when 1 then 'CLUSTERED'
-        else 'NONCLUSTERED'
-        end AS index_type
-     , row_count
-     , ((ps.reserved_page_count * 8024) / 1024 / 1024) as reserved_mb
-     , trace.HostName
-     , trace.LoginName
-     , trace.SPID
-     , trace.ApplicationName
- FROM sys.dm_db_partition_stats ps 
- LEFT JOIN sys.tables  AS o ON o.object_id = ps.OBJECT_ID 
- LEFT JOIN sys.indexes si ON si.object_id = o.object_id AND si.index_id = ps.index_id
- LEFT JOIN ::fn_trace_gettable(
-   ( 
+SELECT * FROM
+(
+SELECT DISTINCT
+ DB_NAME() AS DatabaseName
+, ps.object_id AS ObjectID
+, o.type_desc AS ObjectType
+, o.name AS ObjectName
+, o.create_date AS ObjectCreated
+, si.name AS IndexName
+, CASE si.index_id
+   WHEN 0 THEN 'HEAP'
+   WHEN 1 THEN 'CLUSTERED'
+   ELSE 'NONCLUSTERED'
+   END AS IndexType
+, ps.row_count AS RowsCount
+, ((ps.reserved_page_count * 8024) / 1024 / 1024) AS ReservedMB
+, trace.SPID
+, er.start_time AS RequestStartTime
+, trace.ApplicationName
+, OBJECT_NAME( qt.objectid, qt.dbid ) AS ProcedureName
+, SUBSTRING(CHAR(13) + SUBSTRING (qt.text,(er.statement_start_offset/2) + 1
+   ,((CASE WHEN er.statement_end_offset = -1
+   THEN LEN(CONVERT(NVARCHAR(MAX), qt.text)) * 2 
+   ELSE er.statement_end_offset
+   END - er.statement_start_offset)/2) + 1)
+   ,1,8000) AS StatementText
+, trace.HostName
+, trace.LoginName
+FROM sys.dm_db_partition_stats ps
+JOIN sys.tables  AS o ON o.object_id = ps.OBJECT_ID AND o.is_ms_shipped = 0
+LEFT JOIN sys.indexes si ON si.object_id = o.object_id AND si.index_id = ps.index_id
+LEFT JOIN
+(
+SELECT HostName, LoginName, SPID, ApplicationName, DatabaseName, ObjectID
+   , ROW_NUMBER() OVER (PARTITION BY ObjectID ORDER BY StartTime DESC) MostRecentObjectReference
+FROM fn_trace_gettable(
+   (
    SELECT LEFT(path, LEN(path)-CHARINDEX('\', REVERSE(path))) + '\Log.trc'
-   FROM    sys.traces 
+   FROM    sys.traces
    WHERE   is_default = 1
-   ), DEFAULT) trace
-   ON trace.ObjectID = ps.object_id 
+   ), DEFAULT)
+WHERE ObjectID IS NOT NULL
+) trace
+   ON trace.ObjectID = ps.object_id
    AND trace.DatabaseName = 'tempdb'
-WHERE is_ms_shipped = 0
-ORDER BY reserved_mb DESC;
+   AND MostRecentObjectReference = 1
+LEFT JOIN sys.dm_exec_requests AS er
+       ON er.session_id = trace.SPID
+OUTER APPLY sys.dm_exec_sql_text( er.sql_handle) AS qt
+) AS T
+--WHERE ReservedMB > 0
+ORDER BY ReservedMB DESC;
