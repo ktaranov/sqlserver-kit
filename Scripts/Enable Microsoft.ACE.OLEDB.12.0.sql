715f9594da166fcf72934f15191f1e1a5565d5b9
Konstantin Taranov <kast218@gmail.com>
2017-04-18 21:37:02 +0300

    Fix Ad Hoc Distributed Queries enable script

--- Scripts/Enable Microsoft.ACE.OLEDB.12.0.sql
+++ Scripts/Enable Microsoft.ACE.OLEDB.12.0.sql
@@ -1,8 +1,10 @@
+-- https://social.msdn.microsoft.com/Forums/en-US/1d5c04c7-157f-4955-a14b-41d912d50a64/how-to-fix-error-the-microsoftaceoledb120-provider-is-not-registered-on-the-local-machine
+
 USE master;
 
-sp_configure 'show advanced options', 1;
+EXEC sp_configure 'show advanced options', 1;
 RECONFIGURE;
-sp_configure 'Ad Hoc Distributed Queries', 1;
+EXEC sp_configure 'Ad Hoc Distributed Queries', 1;
 RECONFIGURE;
 GO
 EXEC sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1
