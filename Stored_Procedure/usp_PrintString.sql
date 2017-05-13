b59f63805070f5d164bfb696b7a26af8fdce5d21
Konstantin Taranov <kast218@gmail.com>
2017-05-13 22:07:35 +0300

    Update usp_PrintString for unicode support and SSMS addins format

--- Stored_Procedure/usp_PrintString.sql
+++ Stored_Procedure/usp_PrintString.sql
@@ -2,84 +2,89 @@ IF OBJECT_ID('dbo.usp_PrintString', 'P') IS NULL
     EXECUTE ('CREATE PROCEDURE dbo.usp_PrintString AS SELECT 1');
 GO
 
-ALTER PROCEDURE dbo.usp_PrintString (@str VARCHAR(max))
+ALTER PROCEDURE dbo.usp_PrintString (
+                @str NVARCHAR(MAX)
+)
 AS
+/*
+EXEC dbo.usp_PrintString REPLICATE(¸, 5000);
+*/
 BEGIN
-    DECLARE @line VARCHAR(max)
-        ,@StartLocation INT
-        ,@Length INT
-        ,@TotalLength INT
-        ,@Current INT
+    DECLARE @line          NVARCHAR(MAX)
+          , @StartLocation INT
+          , @Length        INT
+          , @TotalLength   INT
+          , @Current       INT
 
     SELECT @StartLocation = 1
-        ,@TotalLength = datalength(@str)
+        ,@TotalLength = DATALENGTH(@str)
         ,@Current = 1
         ,@Length = 0
 
-    DECLARE @PrintLine NVARCHAR(max) = 
-            'declare @pos int = 1
-            while @pos <= len(@line)
-            begin
-                print substring(@line, @pos, 8000)
-                select @pos = @pos + 8000
-            end'
+    DECLARE @PrintLine NVARCHAR(MAX) = 
+            N'DECLARE @pos int = 1
+            WHILE @pos <= LEN(@line)
+            BEGIN
+                PRINT SUBSTRING(@line, @pos, 4000);
+                SET @pos = @pos + 4000;
+            END;'
 
     WHILE @Current <= @TotalLength
     BEGIN
         IF (
-                substring(@str, @Current, 2) IN (
+                SUBSTRING(@str, @Current, 2) IN (
                     CHAR(0x0D) + CHAR(0x0A)
                     ,CHAR(0x0A) + CHAR(0x0D)
                     )
                 )
         BEGIN
             IF @Length <= 0
-                PRINT ''
+                PRINT '';
             ELSE
             BEGIN -- line
-                SELECT @line = substring(@str, @StartLocation, @Length)
+                SELECT @line = SUBSTRING(@str, @StartLocation, @Length);
 
                 EXEC sp_executesql @PrintLine
-                    ,N'@Line varchar(max)'
-                    ,@line
+                    ,N'@line NVARCHAR(max)'
+                    ,@line;
             END
 
             SELECT @StartLocation = @Current + 2
                 ,@Current = @Current + 2
-                ,@Length = 0
+                ,@Length = 0;
 
             CONTINUE;
         END
         ELSE IF (
-                substring(@str, @Current, 1) IN (
+                SUBSTRING(@str, @Current, 1) IN (
                     CHAR(0x0D)
                     ,CHAR(0x0A)
                     )
                 )
         BEGIN
             IF @Length <= 0
-                PRINT ''
+                PRINT '';
             ELSE
             BEGIN
-                SELECT @line = substring(@str, @StartLocation, @Length)
+                SELECT @line = SUBSTRING(@str, @StartLocation, @Length);
 
                 EXEC sp_executesql @PrintLine
-                    ,N'@Line varchar(max)'
-                    ,@line
+                    ,N'@line NVARCHAR(max)'
+                    ,@line;
             END
 
             SELECT @StartLocation = @Current + 1
                 ,@Current = @Current + 1
-                ,@Length = 0
+                ,@Length = 0;
 
             CONTINUE;
         END
 
         SELECT @Current = @Current + 1
-            ,@Length = @Length + 1
+             , @Length  = @Length  + 1;
     END
 
-    IF (@StartLocation <= datalength(@str))
-        PRINT substring(@str, @StartLocation, datalength(@str))
+    IF (@StartLocation <= DATALENGTH(@str))
+        PRINT SUBSTRING(@str, @StartLocation, DATALENGTH(@str))
 END;
 GO
