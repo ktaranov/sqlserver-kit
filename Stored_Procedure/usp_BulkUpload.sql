36f9e0a187a5dd49377718701f7547696105e323
Konstantin Taranov <kast218@gmail.com>
2017-08-15 09:57:36 +0300

    Comment depends dbo.usp_LogError and dbo.usp_PrintError procedures
    Thanks to francescovista: https://github.com/ktaranov/sqlserver-kit/issues/79

--- Stored_Procedure/usp_BulkUpload.sql
+++ Stored_Procedure/usp_BulkUpload.sql
@@ -210,9 +210,9 @@ IF OBJECT_ID(''tempdb..__#tableName__'') IS NOT NULL DROP TABLE __#tableName__;
         BEGIN
             SET @tsqlCommand = N'ALTER DATABASE __databaseName__ SET RECOVERY __databaseRecoveryMode__ WITH NO_WAIT';
             SET @tsqlCommand = REPLACE(@tsqlCommand, '__databaseName__', @databaseName);
-            SET @tsqlCommand = REPLACE(@tsqlCommand, '__databaseRecoveryMode__', @databaseRecoveryModeCurrent)
+            SET @tsqlCommand = REPLACE(@tsqlCommand, '__databaseRecoveryMode__', @databaseRecoveryModeCurrent);
 
-            IF @debug = 1 PRINT ISNULL('@tsqlCommand = {' + @crlf + @tsqlCommand + @crlf + '}', '@tsqlCommand = {Null}')
+            IF @debug = 1 PRINT ISNULL('@tsqlCommand = {' + @crlf + @tsqlCommand + @crlf + '}', '@tsqlCommand = {Null}');
 
             IF @debug = 0 EXECUTE sp_executesql @tsqlCommand;
         END
@@ -223,11 +223,11 @@ IF OBJECT_ID(''tempdb..__#tableName__'') IS NOT NULL DROP TABLE __#tableName__;
     BEGIN CATCH
         SET @tsqlCommand = N'ALTER DATABASE __databaseName__ SET RECOVERY __databaseRecoveryMode__ WITH NO_WAIT';
         SET @tsqlCommand = REPLACE(@tsqlCommand, '__databaseName__', @databaseName);
-        SET @tsqlCommand = REPLACE(@tsqlCommand, '__databaseRecoveryMode__', @databaseRecoveryModeCurrent)
+        SET @tsqlCommand = REPLACE(@tsqlCommand, '__databaseRecoveryMode__', @databaseRecoveryModeCurrent);
         EXECUTE sp_executesql @tsqlCommand;
 
-        EXECUTE dbo.usp_LogError;
-        EXECUTE dbo.usp_PrintError;
+        -- EXECUTE dbo.usp_LogError;
+        -- EXECUTE dbo.usp_PrintError;
     END CATCH
 END;
 GO
