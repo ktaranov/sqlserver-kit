1507c4600244e9e613effa24da3a3da58bb71a9a
Konstantin Taranov <kast218@gmail.com>
2018-06-09 22:13:03 +0300

    Update Maxdop script

--- Scripts/MAXDOP_recommendation.sql
+++ Scripts/MAXDOP_recommendation.sql
@@ -1,4 +1,6 @@
-/* Use coreinfo (utility by sysinternals) as this will give you
+/* 
+Link: https://dba.stackexchange.com/a/36578/107045
+Use coreinfo (utility by sysinternals) as this will give you
 a. Logical to Physical Processor Map
 b. Logical Processor to Socket Map
 c. Logical Processor to NUMA Node Map as below :
@@ -29,66 +31,67 @@ Considering all above factors, the max degree of Parallelism should be set to 6 
 So the answer is -- "it depends" on your processor footprint and the NUMA configuration and below table will summarize what I explained above:
 8 or less processors    ===> 0 to N (where N= no. of processors)
 More than 8 processors  ===> 8
-NUMA configured         ===> MAXDOP should not exceed no of CPU’s assigned to each 
+NUMA configured         ===> MAXDOP should not exceed no of CPU’s assigned to each
                                  NUMA node with max value capped to 8
 Hyper threading Enabled ===> Should not exceed the number of physical processors.
 Below is a quick and dirty TSQL script to generate Recommendation for MAXDOP setting
 
 Author          :   Kin Shah
 Purpose         :   Recommend MaxDop settings for the server instance
-Tested RDBMS    :   SQL Server 2008R2 */
+Tested RDBMS    :   SQL Server 2008R2, 2014, 2016, 2017 */
 
-declare @hyperthreadingRatio bit
-declare @logicalCPUs int
-declare @HTEnabled int
-declare @physicalCPU int
-declare @SOCKET int
-declare @logicalCPUPerNuma int
-declare @NoOfNUMA int
+DECLARE @hyperthreadingRatio BIT;
+DECLARE @logicalCPUs INT;
+DECLARE @HTEnabled INT;
+DECLARE @physicalCPU INT;
+DECLARE @SOCKET INT;
+DECLARE @logicalCPUPerNuma INT;
+DECLARE @NoOfNUMA INT;
 
-select @logicalCPUs = cpu_count -- [Logical CPU Count]
-    ,@hyperthreadingRatio = hyperthread_ratio --  [Hyperthread Ratio]
-    ,@physicalCPU = cpu_count / hyperthread_ratio -- [Physical CPU Count]
-    ,@HTEnabled = case 
-        when cpu_count > hyperthread_ratio
-            then 1
-        else 0
-        end -- HTEnabled
-from sys.dm_os_sys_info
-option (recompile);
+SELECT @logicalCPUs = cpu_count -- [Logical CPU Count]
+     , @hyperthreadingRatio = hyperthread_ratio --  [Hyperthread Ratio]
+     , @physicalCPU = cpu_count / hyperthread_ratio -- [Physical CPU Count]
+     , @HTEnabled = CASE
+                      WHEN cpu_count > hyperthread_ratio THEN 1
+                      ELSE 0
+                    END -- HTEnabled
+FROM   sys.dm_os_sys_info
+OPTION (recompile);
 
-select @logicalCPUPerNuma = COUNT(parent_node_id) -- [NumberOfLogicalProcessorsPerNuma]
-from sys.dm_os_schedulers
-where [status] = 'VISIBLE ONLINE'
-    and parent_node_id < 64
-group by parent_node_id
-option (recompile);
+SELECT @logicalCPUPerNuma = Count(parent_node_id)
+-- [NumberOfLogicalProcessorsPerNuma]
+FROM   sys.dm_os_schedulers
+WHERE  [status] = 'VISIBLE ONLINE'
+       AND parent_node_id < 64
+GROUP  BY parent_node_id
+OPTION (recompile);
 
-select @NoOfNUMA = count(distinct parent_node_id)
-from sys.dm_os_schedulers -- find NO OF NUMA Nodes 
-where [status] = 'VISIBLE ONLINE'
-    and parent_node_id < 64
+SELECT @NoOfNUMA = Count(DISTINCT parent_node_id)
+FROM   sys.dm_os_schedulers -- find NO OF NUMA Nodes 
+WHERE  [status] = 'VISIBLE ONLINE'
+       AND parent_node_id < 64
 
 -- Report the recommendations ....
-select
-    --- 8 or less processors and NO HT enabled
-    case 
-        when @logicalCPUs < 8
-            and @HTEnabled = 0
-            then 'MAXDOP setting should be : ' + CAST(@logicalCPUs as varchar(3))
-                --- 8 or more processors and NO HT enabled
-        when @logicalCPUs >= 8
-            and @HTEnabled = 0
-            then 'MAXDOP setting should be : 8'
-                --- 8 or more processors and HT enabled and NO NUMA
-        when @logicalCPUs >= 8
-            and @HTEnabled = 1
-            and @NoofNUMA = 1
-            then 'MaxDop setting should be : ' + CAST(@logicalCPUPerNuma / @physicalCPU as varchar(3))
-                --- 8 or more processors and HT enabled and NUMA
-        when @logicalCPUs >= 8
-            and @HTEnabled = 1
-            and @NoofNUMA > 1
-            then 'MaxDop setting should be : ' + CAST(@logicalCPUPerNuma / @physicalCPU as varchar(3))
-        else ''
-        end as Recommendations
+SELECT
+--- 8 or less processors and NO HT enabled
+CASE
+  WHEN @logicalCPUs < 8
+       AND @HTEnabled = 0 THEN 'MAXDOP setting should be : '
+                               + Cast(@logicalCPUs AS VARCHAR(3))
+  --- 8 or more processors and NO HT enabled
+  WHEN @logicalCPUs >= 8
+       AND @HTEnabled = 0 THEN 'MAXDOP setting should be : 8'
+  --- 8 or more processors and HT enabled and NO NUMA
+  WHEN @logicalCPUs >= 8
+       AND @HTEnabled = 1
+       AND @NoOfNUMA = 1 THEN 'MaxDop setting should be : '
+                              + Cast(@logicalCPUPerNuma / @physicalCPU AS
+                              VARCHAR(3))
+  --- 8 or more processors and HT enabled and NUMA
+  WHEN @logicalCPUs >= 8
+       AND @HTEnabled = 1
+       AND @NoOfNUMA > 1 THEN 'MaxDop setting should be : '
+                              + Cast(@logicalCPUPerNuma / @physicalCPU AS
+                              VARCHAR(3))
+  ELSE ''
+END AS Recommendations;
