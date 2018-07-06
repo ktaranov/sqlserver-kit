3048242d5e73b0f1b67ceea89e8428fec3e6a3bd
Konstantin Taranov <kast218@gmail.com>
2018-07-07 00:07:43 +0300

    Add ParameterCaseCheck.ps1 script

---
+++ PowerShell/ParameterCaseCheck.ps1
@@ -0,0 +1,26 @@
+﻿$PatternMatch = '(?<ColumnName>\w+)\s*=\s*@(?<ParameterName>\w+)'
+
+$FindStoredProcedureParameters = @{
+    SqlInstance            = 'localhost'
+    Database               = 'master'
+    Pattern                = $PatternMatch
+    IncludeSystemDatabases = $true
+}
+
+Find-DbaStoredProcedure @FindStoredProcedureParameters |
+    ForEach-Object -Process {
+    $ParentRow = $_
+
+    $_ | ForEach-Object StoredProcedureTextFound | Select-String -Pattern $PatternMatch -AllMatches | ForEach-Object Matches | ForEach-Object -Process {
+        [PSCustomObject]@{
+            ServerName    = $ParentRow.SqlInstance
+            DatabaseName  = $ParentRow.Database 
+            SchemaName    = $ParentRow.Schema
+            ProcedureName = $ParentRow.Name
+            TextFound     = ($_.Groups | Where-Object Name -eq '0').Value
+            ColumnName    = ($_.Groups | Where-Object Name -eq 'ColumnName').Value
+            ParameterName = ($_.Groups | Where-Object Name -eq 'ParameterName').Value
+        }
+    }
+} | Where-Object { ($_.ColumnName -ne $_.ParameterName) } |
+    Select-Object -Property * -Unique
