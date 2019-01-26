e5e46ce6e4197ffed11df773ecbaf37361c381b5
Konstantin Taranov <kast218@gmail.com>
2019-01-26 18:15:02 +0300

    Add How long since you ran DBCC CHECKDB script

---
+++ Scripts/DBCC_CHECKDB_Latest_date.sql
@@ -0,0 +1,72 @@
+/*
+Author: 
+Original link: https://www.sqlserverscience.com/maintenance/check-dbcc-checkdb/
+*/
+
+
+SET NOCOUNT ON;
+
+DECLARE @MaxDaysWithoutCheckDB int = 1;
+
+IF OBJECT_ID(N'tempdb..#Results') IS NOT NULL
+BEGIN
+    DROP TABLE #Results;
+END
+
+CREATE TABLE #Results
+(
+    DatabaseName SYSNAME NULL
+    , IsOnline BIT NULL
+    , ParentObject varchar(100) NULL
+    , [Object] varchar(100) NULL
+    , [Field] varchar(100) NULL
+    , [Value] varchar(100) NULL
+);
+
+DECLARE @cmd NVARCHAR(1000);
+DECLARE @dbName SYSNAME;
+DECLARE @IsOnline BIT;
+
+DECLARE cur CURSOR LOCAL FORWARD_ONLY STATIC READ_ONLY
+FOR
+SELECT DBCCCommand = 'DBCC DBINFO(''' + d.name + ''') WITH TABLERESULTS;'
+    , DatabaseName = d.name
+    , IsOnline = CONVERT(BIT, CASE WHEN d.state_desc = 'ONLINE' THEN 1 ELSE 0 END)
+FROM sys.databases d
+ORDER BY d.name;
+
+OPEN cur;
+FETCH NEXT FROM cur INTO @cmd, @dbName, @IsOnline;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    RAISERROR (@dbName, 0, 1) WITH NOWAIT;
+    IF @IsOnline = 1
+    BEGIN
+        INSERT INTO #Results (ParentObject, [Object], [Field], [Value])
+        EXEC sp_executesql @cmd;
+        UPDATE #Results 
+        SET DatabaseName = @dbName
+            , IsOnline = @IsOnline
+        WHERE DatabaseName IS NULL;
+    END
+    ELSE
+    BEGIN
+        INSERT INTO #Results (DatabaseName, IsOnline)
+        VALUES (@dbName, @IsOnline)
+    END
+
+    FETCH NEXT FROM cur INTO @cmd, @dbName, @IsOnline;
+END
+
+CLOSE cur;
+DEALLOCATE cur;
+
+SELECT ServerName = @@SERVERNAME
+    , DatabaseName = r.DatabaseName
+    , LastKnownGoodDate = CONVERT(DATETIME, r.value, 120)
+    , AtRisk = CASE WHEN DATEDIFF(day, CONVERT(DATETIME, r.value, 120), GETDATE()) > @MaxDaysWithoutCheckDB THEN 'X' ELSE '' END
+    , IsDBOnline = r.IsOnline
+FROM #Results r
+WHERE r.field = 'dbi_dbccLastKnownGood'
+    OR r.field IS NULL;
