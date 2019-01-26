749fe88b8ed0c46d67f3f1c952e1b40d64bd4853
Konstantin Taranov <kast218@gmail.com>
2019-01-26 18:20:30 +0300

    Fix Case-Sensitive and Unicode issues in script

--- Scripts/DBCC_CHECKDB_Latest_date.sql
+++ Scripts/DBCC_CHECKDB_Latest_date.sql
@@ -1,6 +1,7 @@
 /*
-Author: 
+Author: Max Vernon
 Original link: https://www.sqlserverscience.com/maintenance/check-dbcc-checkdb/
+Modified: 2018-01-26 18:20 by Konstantin Taranov
 */
 
 
@@ -11,11 +12,10 @@ DECLARE @MaxDaysWithoutCheckDB int = 1;
 IF OBJECT_ID(N'tempdb..#Results') IS NOT NULL
 BEGIN
     DROP TABLE #Results;
-END
+END;
 
-CREATE TABLE #Results
-(
-    DatabaseName SYSNAME NULL
+CREATE TABLE #Results(
+      DatabaseName SYSNAME NULL
     , IsOnline BIT NULL
     , ParentObject varchar(100) NULL
     , [Object] varchar(100) NULL
@@ -23,13 +23,13 @@ CREATE TABLE #Results
     , [Value] varchar(100) NULL
 );
 
-DECLARE @cmd NVARCHAR(1000);
+DECLARE @cmd NVARCHAR(4000);
 DECLARE @dbName SYSNAME;
 DECLARE @IsOnline BIT;
 
 DECLARE cur CURSOR LOCAL FORWARD_ONLY STATIC READ_ONLY
 FOR
-SELECT DBCCCommand = 'DBCC DBINFO(''' + d.name + ''') WITH TABLERESULTS;'
+SELECT DBCCCommand = CAST(N'DBCC DBINFO(N''' + d.name + N''') WITH TABLERESULTS;' AS NVARCHAR(500))
     , DatabaseName = d.name
     , IsOnline = CONVERT(BIT, CASE WHEN d.state_desc = 'ONLINE' THEN 1 ELSE 0 END)
 FROM sys.databases d
@@ -43,8 +43,10 @@ BEGIN
     RAISERROR (@dbName, 0, 1) WITH NOWAIT;
     IF @IsOnline = 1
     BEGIN
-        INSERT INTO #Results (ParentObject, [Object], [Field], [Value])
+        INSERT INTO #Results
+            (ParentObject, [Object], [Field], [Value])
         EXEC sp_executesql @cmd;
+
         UPDATE #Results 
         SET DatabaseName = @dbName
             , IsOnline = @IsOnline
@@ -52,8 +54,10 @@ BEGIN
     END
     ELSE
     BEGIN
-        INSERT INTO #Results (DatabaseName, IsOnline)
-        VALUES (@dbName, @IsOnline)
+        INSERT INTO #Results
+            (DatabaseName, IsOnline)
+        VALUES
+            (@dbName, @IsOnline)
     END
 
     FETCH NEXT FROM cur INTO @cmd, @dbName, @IsOnline;
@@ -64,9 +68,9 @@ DEALLOCATE cur;
 
 SELECT ServerName = @@SERVERNAME
     , DatabaseName = r.DatabaseName
-    , LastKnownGoodDate = CONVERT(DATETIME, r.value, 120)
-    , AtRisk = CASE WHEN DATEDIFF(day, CONVERT(DATETIME, r.value, 120), GETDATE()) > @MaxDaysWithoutCheckDB THEN 'X' ELSE '' END
+    , LastKnownGoodDate = CONVERT(DATETIME, r.Value, 120)
+    , AtRisk = CASE WHEN DATEDIFF(day, CONVERT(DATETIME, r.Value, 120), GETDATE()) > @MaxDaysWithoutCheckDB THEN 'X' ELSE '' END
     , IsDBOnline = r.IsOnline
 FROM #Results r
-WHERE r.field = 'dbi_dbccLastKnownGood'
-    OR r.field IS NULL;
+WHERE r.Field = 'dbi_dbccLastKnownGood'
+    OR r.Field IS NULL;
