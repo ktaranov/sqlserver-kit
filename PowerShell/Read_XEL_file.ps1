a645327333daf9a6342a3ba4861141bf752fb0fd
Konstantin Taranov <kast218@gmail.com>
2019-02-26 18:26:01 +0300

    Add Read_XEL_file.ps1 powershell script

---
+++ PowerShell/Read_XEL_file.ps1
@@ -0,0 +1,28 @@
+﻿$path = 'd:\system_health_0_131914905113690000.xel'
+ 
+#Add-Type -Path 'C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Extensions\Application\Microsoft.SqlServer.XE.Core.dll'
+#Add-Type -Path 'C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Extensions\Application\Microsoft.SqlServer.XEvent.Linq.dll'
+  
+Add-Type -Path 'C:\Program Files\Microsoft SQL Server\120\Shared\Microsoft.SqlServer.XE.Core.dll'
+Add-Type -Path 'C:\Program Files\Microsoft SQL Server\120\Shared\Microsoft.SqlServer.XEvent.Linq.dll'
+ 
+$events = New-Object Microsoft.SqlServer.XEvent.Linq.QueryableXEventData($path)
+$sb = New-Object System.Text.StringBuilder
+ 
+$events | select -First 50 | %{
+    $event = $_
+    [void]$sb.Append("$($event.Timestamp);;");
+ 
+    for($i=0;$i-lt($event.Fields.Count-1);$i++){  
+        [void]$sb.Append("$($event.Fields[$i].Value.ToString().Replace("`r`n", ''));;");
+    }
+     
+    $event.Actions | %{
+        $action = $_
+        [void]$sb.Append("$($action.value.ToString().Replace("`r`n", ''));;");
+    }
+    [void]$sb.Append("ServerName;;");
+ 
+    [void]$sb.AppendLine();
+}
+$sb.ToString();
