bd38bf9e8190a7f16bee21b42bd49cc74ccab1ac
Konstantin Taranov <kast218@gmail.com>
2019-06-25 18:28:17 +0300

    Improve VLF_Info script
    Fix Msg 2520 for databases with Unicode symbols in name
    Format code
    Output @dbccloginfo information in second data set

--- Scripts/VLF_Info.sql
+++ Scripts/VLF_Info.sql
@@ -3,95 +3,125 @@ https://gallery.technet.microsoft.com/scriptcenter/SQL-Script-to-list-VLF-e63152
 Kev Riley
 */
 
---variables to hold each 'iteration'  
-declare @query varchar(100)  
-declare @dbname sysname  
-declare @vlfs int  
-  
---table variable used to 'loop' over databases  
-declare @databases table (dbname sysname)  
-insert into @databases  
---only choose online databases  
-select name from sys.databases where state = 0  
-  
---table variable to hold results  
-declare @vlfcounts table  
-    (dbname sysname,  
-    vlfcount int)  
-  
- 
- 
---table variable to capture DBCC loginfo output  
---changes in the output of DBCC loginfo from SQL2012 mean we have to determine the version 
- 
-declare @MajorVersion tinyint  
-set @MajorVersion = LEFT(CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max)),CHARINDEX('.',CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max)))-1) 
- 
-if @MajorVersion < 11 -- pre-SQL2012 
-begin 
-    declare @dbccloginfo table  
-    (  
-        fileid smallint,  
-        file_size bigint,  
-        start_offset bigint,  
-        fseqno int,  
-        [status] tinyint,  
-        parity tinyint,  
-        create_lsn numeric(25,0)  
-    )  
-  
-    while exists(select top 1 dbname from @databases)  
-    begin  
-  
-        set @dbname = (select top 1 dbname from @databases)  
-        set @query = 'dbcc loginfo (' + '''' + @dbname + ''') '  
-  
-        insert into @dbccloginfo  
-        exec (@query)  
-  
-        set @vlfs = @@rowcount  
-  
-        insert @vlfcounts  
-        values(@dbname, @vlfs)  
-  
-        delete from @databases where dbname = @dbname  
-  
-    end --while 
-end 
-else 
-begin 
-    declare @dbccloginfo2012 table  
-    (  
-        RecoveryUnitId int, 
-        fileid smallint,  
-        file_size bigint,  
-        start_offset bigint,  
-        fseqno int,  
-        [status] tinyint,  
-        parity tinyint,  
-        create_lsn numeric(25,0)  
-    )  
-  
-    while exists(select top 1 dbname from @databases)  
-    begin  
-  
-        set @dbname = (select top 1 dbname from @databases)  
-        set @query = 'dbcc loginfo (' + '''' + @dbname + ''') '  
-  
-        insert into @dbccloginfo2012  
-        exec (@query)  
-  
-        set @vlfs = @@rowcount  
-  
-        insert @vlfcounts  
-        values(@dbname, @vlfs)  
-  
-        delete from @databases where dbname = @dbname  
-  
-    end --while 
-end 
-  
---output the full list  
-select dbname, vlfcount  
-from @vlfcounts  
-order by dbname
+SET NOCOUNT ON;
+
+/* variables to hold each 'iteration' */
+DECLARE @query  nvarchar(200);
+DECLARE @dbname sysname;
+DECLARE @vlfs   bigint;
+
+/* table variable used to 'loop' over databases */
+DECLARE @databases TABLE (dbname sysname);
+/* only choose online databases */
+INSERT INTO @databases
+SELECT [name] FROM sys.databases WHERE [state] = 0;
+
+/* table variable to hold results */
+declare @vlfcounts table(
+    dbname   sysname
+  , vlfcount int);
+
+--table variable to capture DBCC loginfo output
+--changes in the output of DBCC loginfo from SQL2012 mean we have to determine the version
+
+DECLARE @majorVersion tinyint;
+SET @majorVersion = LEFT(CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max)),
+                         CHARINDEX('.',CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max))) - 1);
+
+/* pre-SQL2012 */
+IF @majorVersion < 11
+BEGIN
+
+    DECLARE @dbccloginfo table(
+        fileid         smallint      NOT NULL,
+        file_size      bigint        NOT NULL,
+        start_offset   bigint        NOT NULL,
+        fseqno         int           NOT NULL,
+        [status]       tinyint       NOT NULL,
+        parity         tinyint       NOT NULL,
+        create_lsn     numeric(25,0) NOT NULL,
+        DBName         sysname NULL,
+        DBID           int     NULL
+    );
+
+    WHILE EXISTS(SELECT TOP(1) dbname FROM @databases)
+    BEGIN
+
+        SET @dbname = (select top 1 dbname from @databases);
+        SET @query = N'DBCC LOGINFO (N' + N'''' + @dbname + N''');';
+
+        INSERT INTO @dbccloginfo(
+             fileid
+           , file_size
+           , start_offset
+           , fseqno
+           , [status]
+           , parity
+           , create_lsn
+        )
+        EXEC sp_executesql @query;
+        SET @vlfs = @@rowcount;
+
+        UPDATE @dbccloginfo
+        SET    DBName = @dbname
+             , DBID  = DB_ID(@dbname)
+        WHERE DBName IS NULL;
+
+        INSERT @vlfcounts(dbname, vlfcount) VALUES(@dbname, @vlfs);
+
+        DELETE FROM @databases WHERE dbname = @dbname;
+
+    END; /* WHILE */
+END;
+ELSE
+BEGIN
+    DECLARE @dbccloginfo2012 table(
+        RecoveryUnitId int           NOT NULL,
+        fileid         smallint      NOT NULL,
+        file_size      bigint        NOT NULL,
+        start_offset   bigint        NOT NULL,
+        fseqno         int           NOT NULL,
+        [status]       tinyint       NOT NULL,
+        parity         tinyint       NOT NULL,
+        create_lsn     numeric(25,0) NOT NULL,
+        DBName         sysname NULL,
+        DBID           int     NULL
+    );
+
+    WHILE EXISTS(SELECT TOP(1) dbname FROM @databases)
+    BEGIN
+
+        SET @dbname = (SELECT TOP(1) dbname FROM @databases);
+        SET @query = N'DBCC LOGINFO (N' + N'''' + @dbname + N''') WITH NO_INFOMSGS;';
+
+        INSERT INTO @dbccloginfo2012(
+             RecoveryUnitId
+           , fileid
+           , file_size
+           , start_offset
+           , fseqno
+           , [status]
+           , parity
+           , create_lsn
+        )
+        EXEC sp_executesql @query;
+        SET @vlfs = @@rowcount;
+
+        UPDATE @dbccloginfo2012
+        SET    DBName = @dbname
+             , DBID  = DB_ID(@dbname)
+        WHERE DBName IS NULL;
+
+        INSERT @vlfcounts(dbname, vlfcount) VALUES(@dbname, @vlfs);
+
+        DELETE FROM @databases WHERE dbname = @dbname;
+
+    END; /* WHILE */
+END;
+
+/* output the full list */
+SELECT dbname, vlfcount
+FROM   @vlfcounts
+ORDER BY vlfcount DESC;
+
+SELECT * FROM @dbccloginfo2012;
