9e13af454db5d286f09a60d3585c5f942dde236d
Konstantin Taranov <kast218@gmail.com>
2019-06-25 19:07:26 +0300

    Cahnge loop to cursor and format code

--- Scripts/VLF_Info.sql
+++ Scripts/VLF_Info.sql
@@ -6,7 +6,7 @@
   <author>Kev Riley</author>
   <created>2016-12-07</created>
   <modified>2019-06-25 by Konstantin Taranov</modified>
-  <version>1.0</version>
+  <version>1.1</version>
   <sourceLink>https://github.com/ktaranov/sqlserver-kit/blob/master/Scripts/VLF_Info.sql</sourceLink>
   <originalLink>https://gallery.technet.microsoft.com/scriptcenter/SQL-Script-to-list-VLF-e6315249</originalLink>
 </documentation>
@@ -16,7 +16,7 @@ SET NOCOUNT ON;
 
 /* variables to hold each 'iteration' */
 DECLARE @query  nvarchar(200);
-DECLARE @dbname sysname;
+DECLARE @dbName sysname;
 
 /* table variable used to 'loop' over databases */
 DECLARE @databases TABLE (dbname sysname);
@@ -28,8 +28,13 @@ DECLARE @majorVersion tinyint;
 SET @majorVersion = LEFT(CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max)),
                          CHARINDEX('.',CAST(SERVERPROPERTY('ProductVersion') AS nvarchar(max))) - 1);
 
+DECLARE DatabaseIteration CURSOR LOCAL STATIC READ_ONLY FORWARD_ONLY FOR
+SELECT [name] AS DatabaseName FROM sys.databases WHERE [state] = 0;
 
 BEGIN
+
+    OPEN DatabaseIteration;
+
     DECLARE @DbccLogInfo TABLE(
         RecoveryUnitId int               NULL,
         fileid         smallint      NOT NULL,
@@ -39,15 +44,17 @@ BEGIN
         [status]       tinyint       NOT NULL,
         parity         tinyint       NOT NULL,
         create_lsn     numeric(25,0) NOT NULL,
-        DBName         sysname NULL,
-        DBID           int     NULL
+        DBName         sysname           NULL,
+        [DBID]         int               NULL
     );
 
-    WHILE EXISTS(SELECT TOP(1) dbname FROM @databases)
+    FETCH NEXT FROM DatabaseIteration INTO @dbName;
+
+    WHILE @@FETCH_STATUS = 0
     BEGIN
 
-        SET @dbname = (SELECT TOP(1) dbname FROM @databases);
-        SET @query = N'DBCC LOGINFO (N' + N'''' + @dbname + N''') WITH NO_INFOMSGS;';
+        SET @query = N'DBCC LOGINFO (N' + N'''' + @dbName + N''') WITH NO_INFOMSGS;';
+        PRINT(@query);
 
         IF @majorVersion < 11 /* pre SQL 2012 versions */
         BEGIN
@@ -78,18 +85,22 @@ BEGIN
         END;
 
         UPDATE @DbccLogInfo
-        SET    DBName = @dbname
-             , DBID  = DB_ID(@dbname)
+        SET    DBName  = @dbName
+             , [DBID]  = DB_ID(@dbName)
         WHERE DBName IS NULL;
 
-        DELETE FROM @databases WHERE dbname = @dbname;
+       FETCH NEXT FROM DatabaseIteration INTO @dbName;
 
-    END; /* WHILE */
+    END;
+
+    CLOSE DatabaseIteration;
+    DEALLOCATE DatabaseIteration;
+
 END;
 
 /* output the full list */
 WITH CTE_VLF AS (
-    SELECT dc.DBID
+    SELECT dc.[DBID]
          , dc.DBName
          , MIN(dc.file_size) / 1024 / 1024 AS FileSizeMbMIN
          , MAX(dc.file_size) / 1024 / 1024 AS FileSizeMbMAX
@@ -97,12 +108,12 @@ WITH CTE_VLF AS (
          , SUM(dc.file_size) / 1024 / 1024 AS FileSizeMbSUM
          , COUNT(dc.fileid)                AS VLFCount
      FROM  @DbccLogInfo AS dc
-     GROUP BY dc.DBID
+     GROUP BY dc.[DBID]
             , dc.DBName
 )
 SELECT   CTE_VLF.*
-       , CASE WHEN VLFCount >= 1000 THEN 'Check your VLF count, maybee you nedd reduce'
-              WHEN FileSizeMbAVG >= 500 THEN 'Check your VLF size, maybe you need decrease it'
+       , CASE WHEN VLFCount      >= 1000 THEN 'Check your VLF count, maybee you need reduce it'
+              WHEN FileSizeMbAVG >= 500  THEN 'Check your VLF size, maybe you need decrease it'
               ELSE 'Maybee it is ok'
          END Warnings
 FROM     CTE_VLF
