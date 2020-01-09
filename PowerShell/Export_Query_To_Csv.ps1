52a49bce11552651435f16079bfa03579c7af708
Konstantin Taranov <kast218@gmail.com>
2020-01-09 18:00:19 +0300

    Fix first bit column issue
    DROP TABLE IF EXISTS t0;
    CREATE TABLE t0 (b bit, c varchar(100), b2 bit, b3 bit);
    INSERT INTO t0 SELECT 1, 'test, test', 1, 1
    UNION ALL SELECT 0, 'test2', 0, 0;

--- PowerShell/Export_Query_To_Csv.ps1
+++ PowerShell/Export_Query_To_Csv.ps1
@@ -3,17 +3,17 @@
 
 <#
 .SYNOPSIS
-    Export SQL server table to csv file with quotes and header
+    Export SQL server table to csv file with quotes (if needed) and header
 
 .DESCRIPTION
-    Piping Invoke-Sqlcmd into csv file and save quotes only if needed.
+    Piping Invoke-Sqlcmd into csv file with quotes only if needed.
     Also replace False and True on 0 and 1 for bit types columns.
 
 .NOTES
    Original link: http://www.sqlmovers.com/removing-quotes-from-csv-created-by-powershell/
    Author: Russ Loski
-   Version: 1.2
-   Modified: 2020-01-07 by Konstantin Taranov
+   Version: 1.3
+   Modified: 2020-01-09 by Konstantin Taranov
    Source link: https://github.com/ktaranov/sqlserver-kit/blob/master/Powershell/Export_Query_To_Csv.ps1
 #>
 
@@ -34,5 +34,7 @@ ConvertTo-CSV -NoTypeInformation `
                -Delimiter $delimiter |
                % {$_ -replace $regReplace, '${start}${output}'} |
                % {$_ -replace ($delimiterBit + 'False'), ($delimiter + '0')} |
+               % {$_ -replace ('^False'), '0'} |
                % {$_ -replace ($delimiterBit + 'True'),  ($delimiter + '1')} |
+               % {$_ -replace ('^True'), '1'} |
 Out-File "$fileName.csv" -fo -en utf8; 
