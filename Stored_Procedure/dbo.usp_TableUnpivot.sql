a64a9a1fa23daeefb828299f2bae3b1718821f3f
Konstantin Taranov <kast218@gmail.com>
2020-01-29 18:28:54 +0300

    Update dbo.usp_TableUnpivot.sql stored procedure

--- Stored_Procedure/dbo.usp_TableUnpivot.sql
+++ Stored_Procedure/dbo.usp_TableUnpivot.sql
@@ -1,76 +1,108 @@
-IF OBJECT_ID('dbo.usp_TableUnpivot', 'P') IS NULL EXECUTE ('CREATE PROCEDURE dbo.usp_TableUnpivot as select 1');
+IF OBJECT_ID('dbo.usp_TableUnpivot', 'P') IS NULL
+EXEC ('CREATE PROCEDURE dbo.usp_TableUnpivot AS SELECT 1');
 go
 
 ALTER PROCEDURE dbo.usp_TableUnpivot (
-                                       @databaseName    SYSNAME       = '',
-                                       @schemaName      SYSNAME       = 'dbo',
-                                       @tableName       SYSNAME,
-                                       @unpivotColumns  NVARCHAR(MAX) = '',
-                                       @constantColumns NVARCHAR(MAX) = '',
-                                       @excludeColumns  NVARCHAR(MAX) = '',
-                                       @orderBYColumns  NVARCHAR(MAX) = '',
-                                       @debug BIT                     = 0
-                                     )
+    @databaseName    sysname       = '',
+    @schemaName      sysname       = 'dbo',
+    @tableName       sysname,
+    @unpivotColumns  nvarchar(max) = '',
+    @constantColumns nvarchar(max) = '',
+    @excludeColumns  nvarchar(max) = '',
+    @orderBYColumns  nvarchar(max) = '',
+    @debug           bit           = 0
+)
 AS
 
 /*
+CREATE TABLE dbo.CustomerPhones
+(
+  CustomerID INT PRIMARY KEY, -- FK
+  Phone1 VARCHAR(32),
+  PhoneType1 CHAR(4),
+  Phone2 VARCHAR(32),
+  PhoneType2 CHAR(4),
+  Phone3 VARCHAR(32),
+  PhoneType3 CHAR(4)
+);
+INSERT INTO dbo.CustomerPhones (CustomerID, Phone1, PhoneType1, Phone2, PhoneType2, Phone3, PhoneType3)
+VALUES
+  (1,'705-491-1111', 'cell', '705-491-1110', 'home', NULL,NULL),
+  (2,'613-492-2222', 'home', NULL, NULL, NULL, NULL),
+  (3,'416-493-3333', 'work', '416-493-3330', 'cell', '416-493-3339', 'home');
+
 EXECUTE [usp_TableUnpivot] @databaseName    = '',
                            @schemaName      = 'dbo',
-                           @tableName       = 'TableName',
-                           @unpivotColumns  = 'UnpivotColumnName',  /*!!! table columns list for unpivot with comma separator without spaces !!!*/
+                           @tableName       = 'CustomerPhones',
+                           @unpivotColumns  = 'CustomerID',  /*!!! table columns list for unpivot with comma separator without spaces !!!*/
                            @excludeColumns  = 'TableNameID',        /*!!! table columns list for excluding with comma separator without spaces !!!*/
                            @constantColumns = ', ''Constant1'' AS ConstantColumnName1, ''Constant2'' AS ConstantColumnName2', /*!!! constant values !!!*/
                            @oderByColumns   = '',
-                           @debug           = 1;                    /*!!! only generate sql statement without executing it !!!*/         
+                           @debug           = 1;                    /*!!! only generate sql statement without executing it !!!*/
 */
 
 BEGIN
 
-DECLARE @tableColumns    NVARCHAR(MAX) = '';
-DECLARE @tableNameFull   NVARCHAR(400) = CASE WHEN @databaseName = '' THEN '' ELSE QUOTENAME(@databaseName) + '.' END + QUOTENAME(@schemaName) + '.' + QUOTENAME(@tableName);
-DECLARE @object_id       INTEGER       = OBJECT_ID(@tableNameFull);
-DECLARE @sqlCommand      NVARCHAR(MAX) = '';
-DECLARE @ParmDefinition  NVARCHAR(500) = '@object_idIN INTEGER, @ColumnsOUT VARCHAR(MAX) OUTPUT';
-DECLARE @crlf            NVARCHAR(5)   = CHAR(13);
+BEGIN TRY
 
-IF @debug = 0 SET NOCOUNT ON ELSE PRINT '/******* Start Debug' + @crlf;
+    DECLARE @tableColumns    nvarchar(max) = '';
+    DECLARE @tableNameFull   nvarchar(400) = CASE WHEN @databaseName = '' THEN '' ELSE QUOTENAME(@databaseName) + '.' END + QUOTENAME(@schemaName) + '.' + QUOTENAME(@tableName);
+    DECLARE @object_id       integer       = OBJECT_ID(@tableNameFull);
+    DECLARE @sqlCommand      nvarchar(max) = '';
+    DECLARE @ParmDefinition  nvarchar(500) = '@object_idIN INTEGER, @ColumnsOUT VARCHAR(MAX) OUTPUT';
+    DECLARE @crlf            nvarchar(5)   = CHAR(13);
 
-IF @debug = 1 PRINT ISNULL('@tableNameFull = {' + CAST(@tableNameFull AS VARCHAR) + '}', '@tableNameFull = Null');
-IF @debug = 1 PRINT ISNULL('@object_id = {' + CAST(@object_id AS VARCHAR) + '}', '@object_id = Null');
+    IF @debug = 0 SET NOCOUNT ON ELSE PRINT '/******* Start Debug' + @crlf;
 
-SET @sqlCommand = CASE WHEN @databaseName = '' THEN '' ELSE N'USE ' + QUOTENAME(@databaseName) + N';' END + @crlf +
-                  N'SELECT @ColumnsOUT  = @ColumnsOUT + QUOTENAME(Name) + '','''                          + @crlf +
-		          N'FROM sys.columns sac '                                                                + @crlf +
-		          N'WHERE sac.object_id = @object_idIN'                                                   + @crlf +
-		          N'      AND Name NOT IN (''' + REPLACE(@unpivotColumns, N',', N''',''') + N''','''
-                                               + REPLACE(@excludeColumns, N',', N''',''') + N''')'        + @crlf +
-		          N'ORDER BY Name;';
+    IF @debug = 1 PRINT ISNULL('@tableNameFull = {' + CAST(@tableNameFull AS VARCHAR) + '}', '@tableNameFull = Null');
+    IF @debug = 1 PRINT ISNULL('@object_id = {' + CAST(@object_id AS VARCHAR) + '}', '@object_id = Null');
 
-IF @debug = 1 PRINT ISNULL(N'@sqlCommand = {' + @crlf + @sqlCommand + @crlf + N'}', N'@sqlCommand = Null');
+    SET @sqlCommand = CASE WHEN @databaseName = '' THEN '' ELSE N'USE ' + QUOTENAME(@databaseName) + N';' END + @crlf +
+                      N'SELECT @ColumnsOUT  = @ColumnsOUT + QUOTENAME(Name) + '','''                          + @crlf +
+                      N'FROM sys.columns sac '                                                                + @crlf +
+                      N'WHERE sac.object_id = @object_idIN'                                                   + @crlf +
+                      N'      AND Name NOT IN (''' + REPLACE(@unpivotColumns, N',', N''',''') + N''','''
+                                                   + REPLACE(@excludeColumns, N',', N''',''') + N''')'        + @crlf +
+                      N'ORDER BY Name;';
 
-EXECUTE sp_executesql @sqlCommand, @ParmDefinition, @object_idIN = @object_id, @ColumnsOUT = @tableColumns OUTPUT SELECT @tableColumns;
+    IF @debug = 1 PRINT ISNULL(N'@sqlCommand = {' + @crlf + @sqlCommand + @crlf + N'}', N'@sqlCommand = Null');
 
-IF @debug = 1 PRINT ISNULL('@tableColumns = {' + @crlf + @tableColumns + @crlf + '}', '@tableColumns = Null');
+    EXECUTE sp_executesql @sqlCommand, @ParmDefinition, @object_idIN = @object_id, @ColumnsOUT = @tableColumns OUTPUT SELECT @tableColumns;
 
-SET @tableColumns = CASE WHEN LEN(@tableColumns) > 0 THEN LEFT(@tableColumns, LEN(@tableColumns) - 1) END;
+    IF @debug = 1 PRINT ISNULL('@tableColumns = {' + @crlf + @tableColumns + @crlf + '}', '@tableColumns = Null');
 
-SET @sqlCommand = N'SELECT ' + @unpivotColumns  + N', val, columns' + @constantColumns + @crlf +
-                  N'  FROM '                                                           + @crlf +
-                  N'      ('                                                           + @crlf +
-                  N'        SELECT *'                                                  + @crlf +
-                  N'          FROM ' + @schemaName + '.' + @tableName                  + @crlf +
-                  N') s'                                                               + @crlf +
-                  N'UNPIVOT'                                                           + @crlf +
-                  N' ('                                                                + @crlf +
-                  N'   val FOR columns IN (' + @tableColumns + ' )'                    + @crlf +
-                  N' ) p'                                                              + @crlf +
-                  CASE WHEN @orderBYColumns != '' THEN N'ORDER BY ' + @orderBYColumns ELSE '' END;
+    SET @tableColumns = CASE WHEN LEN(@tableColumns) > 0 THEN LEFT(@tableColumns, LEN(@tableColumns) - 1) END;
 
-IF @debug = 1 PRINT CAST(ISNULL('@sqlCommand = {' + @crlf + @sqlCommand + @crlf + '}' + @crlf, '@sqlCommand = Null' + @crlf) AS TEXT);
+    SET @sqlCommand = N'SELECT ' + @unpivotColumns  + N', val, columns' + @constantColumns + @crlf +
+                      N'  FROM '                                                           + @crlf +
+                      N'      ('                                                           + @crlf +
+                      N'        SELECT *'                                                  + @crlf +
+                      N'          FROM ' + @schemaName + '.' + @tableName                  + @crlf +
+                      N') s'                                                               + @crlf +
+                      N'UNPIVOT'                                                           + @crlf +
+                      N' ('                                                                + @crlf +
+                      N'   val FOR columns IN (' + @tableColumns + ' )'                    + @crlf +
+                      N' ) p'                                                              + @crlf +
+                      CASE WHEN @orderBYColumns != '' THEN N'ORDER BY ' + @orderBYColumns ELSE '' END;
+    
+    IF @debug = 1 PRINT CAST(ISNULL('@sqlCommand = {' + @crlf + @sqlCommand + @crlf + '}' + @crlf, '@sqlCommand = Null' + @crlf) AS TEXT);
+    
+    IF @debug = 0 EXECUTE sp_executesql @sqlCommand;
+    
+    IF @debug = 0 SET NOCOUNT OFF ELSE PRINT 'End Debug *******/';
+    END TRY
 
-IF @debug = 0 EXECUTE sp_executesql @sqlCommand;
-
-IF @debug = 0 SET NOCOUNT OFF ELSE PRINT 'End Debug *******/';
+    BEGIN CATCH
+        PRINT(@crlf +
+            'Error: '     + CONVERT(varchar(50), ERROR_NUMBER())  +
+          ', Severity: '  + CONVERT(varchar(5), ERROR_SEVERITY()) +
+          ', State: '     + CONVERT(varchar(5), ERROR_STATE())    +
+          ', Procedure: ' + ISNULL(ERROR_PROCEDURE(), '-')        +
+          ', Line: '      + CONVERT(varchar(5), ERROR_LINE())     +
+          ', User name: ' + CONVERT(sysname, ORIGINAL_LOGIN())
+          );
+        PRINT(ERROR_MESSAGE() + @crlf + @crlf);
+    END CATCH;
 
 END
-go
+GO
