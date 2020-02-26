9c53c6c5668f61b4430fc91dcc26b782065eb276
Konstantin Taranov <kast218@gmail.com>
2020-02-26 17:05:21 +0300

    Update InvestigateWaitsMaxDop extended event

--- Extended_Events/InvestigateWaitsMaxDop.sql
+++ Extended_Events/InvestigateWaitsMaxDop.sql
@@ -2,6 +2,10 @@
 Original link: https://www.sqlskills.com/blogs/paul/who-is-overriding-maxdop-1-on-the-instance/
 Author: Paul Randal
 
+-- Get parallel plan with trace flag https://github.com/ktaranov/sqlserver-kit/blob/master/SQL%20Server%20Trace%20Flag.md#8649
+SELECT * FROM AdventureWorks2012.Sales.SalesOrderDetail WHERE OrderQty > 1000 OPTION (MAXDOP 8, QUERYTRACEON 8649);
+GO
+
 SELECT
     [data1].[value] ('(./@timestamp)[1]', 'datetime') AS [Time],
     [data1].[value] ('(./data[@name="dop"]/value)[1]', 'INT') AS [DOP],
@@ -13,7 +17,7 @@ FROM (
     FROM sys.dm_xe_session_targets [xst]
     INNER JOIN sys.dm_xe_sessions [xs]
         ON [xst].[event_session_address] = [xs].[address]
-    WHERE [xs].[name] = N'InvestigateWaits') AS t
+    WHERE [xs].[name] = N'InvestigateWaitsMaxDop') AS t
 CROSS APPLY data.nodes('//event') n (data1);
 GO
 */
@@ -28,15 +32,15 @@ IF EXISTS (SELECT * FROM sys.server_event_sessions WHERE name = 'InvestigateWait
 
 /* Create trace */
 CREATE EVENT SESSION InvestigateWaitsMaxDop ON SERVER
-ADD EVENT [sqlserver].[degree_of_parallelism]
+ADD EVENT sqlserver.degree_of_parallelism
 (
     ACTION (
         sqlserver.client_hostname,
         sqlserver.nt_username,
         sqlserver.sql_text)
-    WHERE [dop] > 1 /* parallel plans */
+    WHERE dop > 1 /* parallel plans */
 )
-ADD TARGET [package0].[ring_buffer]
+ADD TARGET package0.ring_buffer
 WITH (
     MAX_MEMORY = 50 MB,
     MAX_DISPATCH_LATENCY = 5 SECONDS
@@ -45,4 +49,5 @@ GO
 
 /* Start trace
 ALTER EVENT SESSION InvestigateWaitsMaxDop ON SERVER STATE = START;
+GO
 */
