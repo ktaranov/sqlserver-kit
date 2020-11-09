9037d4d861344557699767a127fd352d99668acd
Konstantin Taranov <kast218@gmail.com>
2020-11-09 14:27:26 +0300

    Add Disable_CEIP.ps1 script

---
+++ PowerShell/Disable_CEIP.ps1
@@ -0,0 +1,21 @@
+$server = 'Server01' ,'Server02','Server03','Server04';
+ 
+# Stop all CEIP services
+invoke-command -computername $server {Get-Service |? name -Like "*TELEMETRY*" | ? status -eq "running" | Stop-Service};
+ 
+# Disable all CEIP services
+invoke-command -computername $server {Get-Service |? name -Like "*TELEMETRY*" | Set-Service -StartMode Disabled};
+ 
+# Check service status (should see "stopped" and "disabled"
+invoke-command -computername $server {Get-Service |? name -Like "*TELEMETRY*" | select -property name,starttype,status};
+ 
+# Disable "Error Reporting" and "Customer Feedback" Registry Values
+invoke-command -computername $server {Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server' -Recurse | Where-Object -Property Property -eq 'EnableErrorReporting'| Set-ItemProperty -Name EnableErrorReporting -Value 0};
+invoke-command -computername $server {Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server' -Recurse | Where-Object -Property Property -eq 'EnableErrorReporting'| Set-ItemProperty -Name CustomerFeedback -Value 0};
+invoke-command -computername $server {Get-ChildItem 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Microsoft SQL Server' -Recurse | Where-Object -Property Property -eq 'EnableErrorReporting'| Set-ItemProperty -Name EnableErrorReporting -Value 0};
+invoke-command -computername $server {Get-ChildItem 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Microsoft SQL Server' -Recurse | Where-Object -Property Property -eq 'EnableErrorReporting'| Set-ItemProperty -Name CustomerFeedback -Value 0};
+ 
+# Check registry values (should see 0 for "EnableErrorReporting" and "CustomerFeedback")
+invoke-command -computername $server {Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server' -Recurse | Where-Object -Property Property -eq 'EnableErrorReporting'};
+invoke-command -computername $server {Get-ChildItem 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Microsoft SQL Server' -Recurse | Where-Object -Property Property -eq 'EnableErrorReporting'};
+ 
